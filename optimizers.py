b56eb1d86668ff8c00b15ce3bb249ed4859b2051
shoutrix <dasshoutrik@gmail.com>
2025-03-08 17:23:45 +0530

    bug fixes

--- optimizers.py
+++ optimizers.py
@@ -118,6 +118,9 @@ class NAdam:
         self.first_moment = {id(param): np.zeros_like(param.data) for param in self.params}
         self.second_moment = {id(param): np.zeros_like(param.data) for param in self.params}
         self.t = 0
+        
+        
+        print("NADAM : ", self.lr, self.eps, self.weight_decay, self.beta1, self.beta2)
 
     def step(self):
         self.t += 1
--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+# from torch_trainer import Trainer
 from trainer import Trainer
 import argparse
 import wandb
@@ -13,21 +14,21 @@ def parse_args():
     parser.add_argument("-d", "--dataset", type=str, choices=["mnist", "fashion_mnist"], required=False, default="fashion_mnist")
 
     parser.add_argument("-e", "--epochs", type=int, default=10, help="Number of epochs")
-    parser.add_argument("-b", "--batch_size", type=int, default=64, help="Batch size")
+    parser.add_argument("-b", "--batch_size", type=int, default=32, help="Batch size")
     parser.add_argument("-l", "--loss", type=str, choices=["mean_squared_error", "cross_entropy"], required=False, default="cross_entropy")
-    parser.add_argument("-o", "--optimizer", type=str, choices=["sgd", "momentum", "nag", "rmsprop", "adam", "nadam"], required=False, default="adam")
+    parser.add_argument("-o", "--optimizer", type=str, choices=["sgd", "momentum", "nag", "rmsprop", "adam", "nadam"], required=False, default="nadam")
     parser.add_argument("-lr", "--learning_rate", type=float, default=1e-3, help="Learning rate")
     parser.add_argument("-m", "--momentum", type=float, default=0.5, help="Momentum (for momentum-based optimizers)")
     parser.add_argument("-beta", "--beta", type=float, default=0.5, help="Beta (for RMSprop)")
-    parser.add_argument("-beta1", "--beta1", type=float, default=0.5, help="Beta1 (for Adam/Nadam)")
-    parser.add_argument("-beta2", "--beta2", type=float, default=0.5, help="Beta2 (for Adam/Nadam)")
-    parser.add_argument("-eps", "--epsilon", type=float, default=1e-6, help="Epsilon for numerical stability")
-    parser.add_argument("-w_d", "--weight_decay", type=float, default=1e-4, help="Weight decay (L2 regularization)")
+    parser.add_argument("-beta1", "--beta1", type=float, default=0.9, help="Beta1 (for Adam/Nadam)")
+    parser.add_argument("-beta2", "--beta2", type=float, default=0.999, help="Beta2 (for Adam/Nadam)")
+    parser.add_argument("-eps", "--epsilon", type=float, default=1e-8, help="Epsilon for numerical stability")
+    parser.add_argument("-w_d", "--weight_decay", type=float, default=5e-3, help="Weight decay (L2 regularization)")
 
-    parser.add_argument("-w_i", "--weight_init", type=str, choices=["random", "Xavier", "kaiming"], default="kaiming", help="Weight initialization method")
-    parser.add_argument("-nhl", "--num_layers", type=int, default=4, help="Number of hidden layers")
+    parser.add_argument("-w_i", "--weight_init", type=str, choices=["random", "Xavier", "kaiming", "Xavier_normal", "kaiming_normal"], default="Xavier_normal", help="Weight initialization method")
+    parser.add_argument("-nhl", "--num_layers", type=int, default=3, help="Number of hidden layers")
     parser.add_argument("-sz", "--hidden_size", type=int, default=256, help="Hidden layer size")
-    parser.add_argument("-a", "--activation", type=str, choices=["identity", "sigmoid", "tanh", "ReLU"], required=False, default="ReLU")
+    parser.add_argument("-a", "--activation", type=str, choices=["identity", "sigmoid", "tanh", "ReLU"], required=False, default="tanh")
 
     return parser.parse_args()
 
